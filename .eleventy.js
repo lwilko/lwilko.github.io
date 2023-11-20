a6f504b7716e94ec515b69b3fea30a5179bf04ab
lwilko <lily.wilkinson@live.co.uk>
2023-11-20 21:12:12 +0000

    First fully functional version

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,10 @@
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addNunjucksFilter("currentYear", function() {
+    return new Date().getFullYear();
+  });
+
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/styles");
 
   return {
     dir: {
